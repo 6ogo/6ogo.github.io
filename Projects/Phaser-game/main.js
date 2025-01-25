d150f9c5dae2c172a9aa7ddd046a845db0730f35
George <106427405+6ogo@users.noreply.github.com>
2025-01-25 15:50:11 +0100

    Update main.js

--- Projects/Phaser-game/main.js
+++ Projects/Phaser-game/main.js
@@ -1,3 +1,4 @@
+// Projects/Phaser-Game/main.js
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -34,49 +35,21 @@ function preload() {
 }
 
 function create() {
-    // Background
     this.add.image(400, 300, 'background');
-
-    // Platforms
     platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-    // Player
     player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    // Player animations
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    // Collide player with platforms
-    this.physics.add.collider(player, platforms);
-
-    // Input
     cursors = this.input.keyboard.createCursorKeys();
-
-    // Enemies
+    
     enemies = this.physics.add.group();
     createEnemy(this, 600, 500);
+    
     this.physics.add.collider(enemies, platforms);
+    this.physics.add.collider(player, platforms);
     this.physics.add.collider(player, enemies, hitPlayer, null, this);
 }
 
@@ -91,19 +64,14 @@ function update() {
         return;
     }
 
-    if (dashCooldown > 0) {
-        dashCooldown--;
-    }
+    if (dashCooldown > 0) dashCooldown--;
 
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
-        player.anims.play('left', true);
     } else if (cursors.right.isDown) {
         player.setVelocityX(160);
-        player.anims.play('right', true);
     } else {
         player.setVelocityX(0);
-        player.anims.play('turn');
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
